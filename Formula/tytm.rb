bf7e6961158019ccc7d0b631e96c9c6b73b2043a
Aaron Chen <122733139+chen1plus@users.noreply.github.com>
2026-07-05 16:25:27 +0800

    Update TyTM to 0.5.2

--- Formula/tytm.rb
+++ Formula/tytm.rb
@@ -4,8 +4,8 @@ class Tytm < Formula
   license "MIT"
 
   head "https://github.com/chen1plus/tytm.git", branch: "master"
-  sha256 "386722019a7d80a045bc96e061f651fbfcbe5e6df78819f31ad9481b9782eae2"
-  url "https://github.com/chen1plus/tytm/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "0a54122eeb52b6ce30ced7c61118314d56e629d7ea312b2de1029cf30e510fbd"
+  url "https://github.com/chen1plus/tytm/archive/refs/tags/v0.5.2.tar.gz"
 
   depends_on "rust" => :build
   def install
