7bb180f707e3c877120d500c8ad8584ebf111397
Aaron Chen <122733139+chen1plus@users.noreply.github.com>
2026-07-12 11:48:29 +0800

    Update Tong Wen to 0.2.1

--- Formula/tongwen.rb
+++ Formula/tongwen.rb
@@ -4,8 +4,8 @@ class Tongwen < Formula
   license "MIT"
 
   head "https://github.com/chen1plus/tongwen.git", branch: "main"
-  sha256 "03cfd355f4661851f57a00a592f1c9ae0e487942540981de2536fcdaa6386d59"
-  url "https://github.com/chen1plus/tongwen/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "7df66240ff51f4e16b93f66ae0df8b143cb1d11572ab27ec3cd54a6a73907f84"
+  url "https://github.com/chen1plus/tongwen/archive/refs/tags/v0.2.1.tar.gz"
 
   depends_on "rust" => :build
   def install
