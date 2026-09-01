b92c7e6d29eb7548b99cbde98e8f3dce53a921cc
Aaron Chen <122733139+chen1plus@users.noreply.github.com>
2026-09-02 00:18:27 +0800

    Small tweaks

--- Formula/tongwen.rb
+++ Formula/tongwen.rb
@@ -1,14 +1,15 @@
 class Tongwen < Formula
-  desc "An OpenAI-compatible local Chinese Converter endpoint."
+  desc "An OpenAI compatible Chinese converter"
   homepage "https://github.com/chen1plus/tongwen"
   license "MIT"
+
+  depends_on "opencc"
+  depends_on "rust" => :build
 
   head "https://github.com/chen1plus/tongwen.git", branch: "main"
   sha256 "5d0376c6f19bd6bf6785eaef1fa30200cb14c592ecaee14c4fcc14334b5e2907"
   url "https://github.com/chen1plus/tongwen/archive/refs/tags/v0.3.0.tar.gz"
 
-  depends_on "rust" => :build
-  depends_on "opencc"
   def install
     system "cargo", "install", *std_cargo_args
   end
--- README.md
+++ README.md
@@ -4,9 +4,5 @@ This is my personal Homebrew tap. To add the tap, run:
 
 ```sh
 brew tap chen1plus/tap
+brew trust chen1plus/tap
 ```
-
-The tap includes:
-
-- `tongwen` — an OpenAI-compatible local Chinese Converter endpoint.
-- `tytm` — Typora theme manager.
